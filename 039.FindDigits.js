643532a276ad020c5a5939aa3bdd85c07b6ada58
mhshuvoit <mhshuvoit@gmail.com>
2021-08-26 20:32:12 +0600

    before windows

---
+++ 039.FindDigits.js
@@ -0,0 +1,12 @@
+function findDigits(n) {
+    let result = 0;
+    let digits = n.toString().split('');
+    for (let i = 0; i < digits.length; i++) {
+        if (n % Number.parseInt(digits[i]) == 0) {
+            result++;
+        }
+    }
+    console.log(result);
+}
+
+findDigits(1012);
